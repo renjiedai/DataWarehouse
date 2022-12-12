26bf29e1640bd92f4bd4df56bca71966f95aad4b
renjiedai <2312617455@qq.com>
2022-12-12 12:02:34 +0800

    更新依赖和配置

---
+++ DWBackend/src/main/java/cn/edu/tongji/dwbackend/configuration/AppConfiguration.java
@@ -0,0 +1,20 @@
+package cn.edu.tongji.dwbackend.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AppConfiguration {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*");
+            }
+        };
+    }
+}
