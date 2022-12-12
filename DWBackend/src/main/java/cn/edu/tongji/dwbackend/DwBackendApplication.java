ea0cfe1b45704060064904ed0942e61d1be8fbbf
renjiedai <2312617455@qq.com>
2022-12-12 11:52:01 +0800

    init

---
+++ DWBackend/src/main/java/cn/edu/tongji/dwbackend/DwBackendApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.tongji.dwbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DwBackendApplication.class, args);
+    }
+
+}
---
+++ DWBackend/src/test/java/cn/edu/tongji/dwbackend/DwBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.tongji.dwbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DwBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
